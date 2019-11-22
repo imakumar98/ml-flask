e743d26f96a224e66eb1f62dd3af033f6e9abce4
Ashwani <imakumar98@gmail.com>
2019-11-22 16:43:55 +0530

    Create basic template of project

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def hello_www():
-    return "Hello World Wide Web!"
+    # return '<h1>Hello World</h1>'
+    return render_template('index.html')
 
 
 app.run()
